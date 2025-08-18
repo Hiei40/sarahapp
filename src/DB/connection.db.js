56e6a751ab873afd8199aeba8c2ed444a55e6384
Moka <mohamedmoka662@gmail.com>
2025-08-18 21:16:55 +0300

    Update database connection URL and environment variable name for consistency

--- src/DB/connection.db.js
+++ src/DB/connection.db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     // const uri = process.env.Dburl;
-    console.log(process.env.Dburl);
+    console.log(process.env.DB_URL);
     
-    const result = await mongoose.connect(process.env.Dburl)
+    const result = await mongoose.connect(process.env.DB_URL)
     console.log(result.models);
     console.log(`DB connected successfully ❤️`);
   } catch (error) {
